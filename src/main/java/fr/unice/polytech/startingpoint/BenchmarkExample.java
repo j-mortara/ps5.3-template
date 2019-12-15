36a9f50006d6624784c914a6fc68ffe2ac9c0493
Philippe Collet <Philippe.Collet@unice.fr>
2019-12-15 12:33:18 +0100

    pom updated for last versions of stack + JMH 1.2 and Benchmark example Watchout for execution: mvn exec:java@run for the Main / mvn exec:exec@bench for the benchmark example

---
+++ src/main/java/fr/unice/polytech/startingpoint/BenchmarkExample.java
@@ -0,0 +1,65 @@
+package fr.unice.polytech.startingpoint;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@State(Scope.Benchmark)
+@Fork(value = 2, jvmArgs = {"-Xms2G", "-Xmx2G"})
+@Warmup(iterations = 2)
+@Measurement(iterations = 5)
+public class BenchmarkExample {
+
+    @Param({"10000000"})
+    private int N;
+
+    private List<String> DATA_FOR_TESTING;
+
+    public static void main(String[] args) throws RunnerException {
+
+        Options opt = new OptionsBuilder()
+                .include(BenchmarkExample.class.getSimpleName())
+                .forks(1)
+                .build();
+
+        new Runner(opt).run();
+    }
+
+    @Setup
+    public void setup() {
+        DATA_FOR_TESTING = createData();
+    }
+
+    @Benchmark
+    public void loopFor(Blackhole bh) {
+        for (int i = 0; i < DATA_FOR_TESTING.size(); i++) {
+            String s = DATA_FOR_TESTING.get(i); //take out n consume, fair with foreach
+            bh.consume(s);
+        }
+    }
+
+    @Benchmark
+    public void loopForEach(Blackhole bh) {
+        for (String s : DATA_FOR_TESTING) {
+            bh.consume(s);
+        }
+    }
+
+    private List<String> createData() {
+        List<String> data = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            data.add("Number : " + i);
+        }
+        return data;
+    }
+
+}
